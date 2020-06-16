f74c4188c1977591cf56c00bfe5eafa46ee1e8cd
袁咪 <yuanmi@yuanmideMacBook-Pro.local>
2020-06-16 21:56:07 +0800

    滚动跟随

--- src/common/js/dom.js
+++ src/common/js/dom.js
@@ -9,3 +9,31 @@ export function getData(el, name, val) {
     return el.getAttribute(prefix+name)
   }
 }
+
+// 浏览器兼容
+let elementStyle = document.createElement('div').style
+
+let vander = (() => {
+  let transformNames = {
+    webkit: 'webkitTransform',
+    Moz: 'MozTransform',
+    O: 'OTransform',
+    ms: 'msTransform',
+    standard: 'transform'
+  }
+
+  for (let key in transformNames) {
+    if (elementStyle[transformNames[key]] !== undefined) {
+      return key
+    }
+  }
+
+  return false
+})()
+
+export function prefixStyle(style) {
+  if (vander === false || vander === 'standard') {
+    return false
+  }
+  return vander + style.charAt(0).toUpperCase()+style.slice(1)
+}
